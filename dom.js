3e24484cdb4293080afe99276f35dbd5ddc11364
sadik_shaik <sadikshaik139@gmail.com>
2021-12-03 06:26:42 +0530

    changed header title color to red

--- dom.js
+++ dom.js
@@ -2,3 +2,5 @@ const head = document.getElementById('head')
 head.textContent = "Hello this sadik :)"
 head.style.backgroundColor = "blue";
 head.style.color = "white";
+
+head.style.border = "2px solid red"
